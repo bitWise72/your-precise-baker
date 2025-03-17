fe08e385e4a7647a66175c4b5cc9f40d563e2566
Kaustav Das <kaustavdas0228@gmail.com>
2025-03-17 11:35:34 +0530

    auth page

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,7 @@ import Index from "./pages/Index"
 import NotFound from "./pages/NotFound"
 import ReviewPost from "./pages/ReviewPost"
 import Community from "./pages/Community"
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-import GoogleAuthProviderWrapper from "./context/GoogleAuthProvider";
-import { Login } from "./components/Login";
-
+import { Login } from "./components/Login"
 const queryClient = new QueryClient()
 
 const App = () => (
@@ -29,7 +20,8 @@ const App = () => (
         <Sonner />
         <Router>
           <Routes>
-            <Route path="/" element={<Index />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Index />}/>
             <Route path="/review-post" element={<ReviewPost />} />
             <Route path="/community" element={<Community />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
@@ -38,21 +30,7 @@ const App = () => (
         </Router>
       </TooltipProvider>
     </DarkModeProvider>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={< Login/>} />
-          <Route path="/home" element={<Index/>}/>
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
   </QueryClientProvider>
 )
-  // </GoogleAuthProviderWrapper>
-);
 
 export default App
