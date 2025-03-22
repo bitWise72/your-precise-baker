9f69ddd74a8d910a068be2194ba9d978bd232ba2
Kaustav Das <kaustavdas0228@gmail.com>
2025-03-22 15:52:38 +0530

    default path add

--- backend/server.ts
+++ backend/server.ts
@@ -31,5 +31,9 @@ app.use(passport.session())
 
 app.use("/auth", authRoutes)
 
+app.get("/",(req,res)=>{
+  res.send("Backend is working")
+})
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
