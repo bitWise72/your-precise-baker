21cc3700885368e3fffdb6dc461d32d52a300e97
Sayan Ganguly <155835726+bitWise72@users.noreply.github.com>
2025-03-23 18:52:35 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,13 +15,14 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "build"
+  }
 }));
-
